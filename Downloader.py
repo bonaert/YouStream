26a6bdc4e877c380df9e47f6a803213028429608
harokb <harokb@gmail.com>
2013-11-09 17:10:55 +0000

    State desired functions of downloader.

---
+++ Downloader.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+class Downloader():
+    def __init__(self):
+        pass
+
+    def download_next_song(self):
+        pass
+
+    def stop_download(self):
+        pass
+
+    def download(self, index):
+        pass
+
+    def start_prefetching(self):
+        pass
+
+    def start_prefetching_future_videos(self):
+        pass
+
+    def is_there_song_to_download(self):
+        pass
+
+    def get_song_url(self, index):
+        pass
+
+    def get_current_song_file_path(self):
+        pass
+
+    def get_current_song_title(self):
+        pass
+
+    def get_current_song_length(self):
+        pass
+
+    def is_downloading(self):
+        pass
