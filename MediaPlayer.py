8235f70eb66cc80837ca7eb87d6b74033f1757fb
harokb <harokb@gmail.com>
2013-11-16 16:30:10 +0000

    Added some error checking.

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -44,7 +44,11 @@ class MediaPlayer(object):
         self.media_player.Loop(0)
 
     def get_current_video_time(self):
-        return self.media_player.GetTimePos()
+        time_position = self.media_player.GetTimePost()
+        if time_position:
+            return int(time_position)
+        else:
+            return 0
 
     def get_current_video_length(self):
         length = self.media_player.GetTimeLength()
